e6332b83a3621adec0ca682651e56fc5f832a870
Agustín Avellaneda <100479647+nitdraig@users.noreply.github.com>
2023-01-01 18:16:12 -0300

    1

--- src/main/java/com/portfolioA/A/Draig/Controller/CEducacion.java
+++ src/main/java/com/portfolioA/A/Draig/Controller/CEducacion.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = {"https://a-agustin-portfolio-ap-68352.web.app/", "http://localhost:4200"})
+@CrossOrigin(origins = {"https://a-agustin-portfolio-ap-68352.web.app", "http://localhost:4200"})
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
